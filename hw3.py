bba4a91b4e0579d8c9d4c2e0536951370bd1a9c9
Rachel Lacobelle <rlacobelle02@gmail.com>
2024-01-23 18:22:53 -0500

    added parse function

--- hw3.py
+++ hw3.py
@@ -1,5 +1,12 @@
 import json
 import graphviz
 
-def parse():
+# parse json file
+def parse(j_file):
+    fp = open(j_file) # opens json file into fp
+    data = json.load(fp) # returns json file as dicttionary
+    return data, fp
+
+def digraph():
     return
+
