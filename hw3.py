d04e4c3fecde2ad0ea069caab12cc4817686b440
Rachel Lacobelle <rlacobelle02@gmail.com>
2024-01-23 19:06:57 -0500

    updated

--- hw3.py
+++ hw3.py
@@ -26,55 +26,55 @@ def digraph(j_file):
     edges = [] # holds all prereq data
     
     # add CS1
-    dot.node('CSCI 1100','CSCI 1100 || Computer Science I')
+    dot.node('CSCI 1100','Computer Science I')
     node_num.append('CSCI 1100')
-    node_name.append('CSCI 1100 || Computer Science I')
+    node_name.append('Computer Science I')
     
     # major required
     for i in range(7):
         course = data_csci[4]['rules'][0]['rule_data']['rules'][i]['rule_data']['courses'][0]['dept'] + ' ' + data_csci[4]['rules'][0]['rule_data']['rules'][i]['rule_data']['courses'][0]['crse']
-        dot.node(course,course + " || " + course,data_csci[4]['rules'][0]['rule_data']['rules'][i]['label'])
+        dot.node(course,data_csci[4]['rules'][0]['rule_data']['rules'][i]['label'])
         node_num.append(course)
-        node_name.append(course + " || " +data_csci[4]['rules'][0]['rule_data']['rules'][i]['label'])
+        node_name.append(data_csci[4]['rules'][0]['rule_data']['rules'][i]['label'])
     
     
     # math/science core
     # intro to bio/intro to bio lab
-    dot.node('BIOL 1010','BIOL 1010 || Intro to Biology')
+    dot.node('BIOL 1010','Intro to Biology')
     node_num.append('BIOL 1010')
-    node_name.append('BIOL 1010 || Intro to Biology')
+    node_name.append('Intro to Biology')
     
-    dot.node('BIOL 1015','BIOL 1015 || Intro to Biology Laboratory')
+    dot.node('BIOL 1015','Intro to Biology Laboratory')
     node_num.append('BIOL 1015')
-    node_name.append('BIOL 1015 || Intro to Biology Laboratory')
+    node_name.append('Intro to Biology Laboratory')
     
     edges.append(('BIOL 1015','BIOL 1010'))
     dot.edge('BIOL 1010','BIOL 1015')
     
     # physics 1
-    dot.node('PHYS 1100','PHYS 1100 || Physics I')
+    dot.node('PHYS 1100','Physics I')
     node_num.append('PHYS 1100')
-    node_name.append('PHYS 1100 || Physics I')
+    node_name.append('Physics I')
     
     # science elective
-    dot.node('SCIOP', 'SCIOP || Science Option')
+    dot.node('SCIOP', 'Science Option')
     node_num.append('SCIOP')
-    node_name.append('SCIOP || Science Option')
+    node_name.append('Science Option')
     
     # calc 1
-    dot.node('MATH 1010','MATH 1010 || Calculus I')
+    dot.node('MATH 1010','Calculus I')
     node_num.append('MATH 1010')
-    node_name.append('MATH 1010 || Calculus I')
+    node_name.append('Calculus I')
     
     # calc 2
-    dot.node('MATH 1020','MATH 1020 || Calculus I')
+    dot.node('MATH 1020','Calculus I')
     node_num.append('MATH 1020')
-    node_name.append('MATH 1020 || Calculus I')
+    node_name.append('Calculus I')
     
     # math elective 1
-    dot.node('MATH 1', 'MATH 1 || Mathematics Options I')
+    dot.node('MATH 1', 'Mathematics Options I')
     node_num.append('MATH 1')
-    node_name.append('MATH 1 || Mathematics Options I')
+    node_name.append('Mathematics Options I')
     
     # math elective 2
 
@@ -106,6 +106,7 @@ def digraph(j_file):
     dot.render(directory='doctest-output', view=True) 
     
     fp.close()
+    fp1.close()
 
 
 # required
